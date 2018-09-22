9559bc83f035c72596b173edd2f7061da01ad3eb
abekjarovska <a.bekjarovska@betclicgroup.com>
2018-09-22 15:39:44 +0200

    Add BooksController

---
+++ BookShopAPI/BookShopAPI/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private IRepositoryWrapper _repoWrapper;
+
+        public BooksController(IRepositoryWrapper repoWrapper)
+        {
+            _repoWrapper = repoWrapper;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public ActionResult<IList<Books>> Get()
+        {
+
+            var books = _repoWrapper.Books.FindAll();
+            return Ok(books);
+        }
+
+        // POST: api/Books
+        [HttpPost]
+        public void Post([FromBody] Books book)
+        {
+            _repoWrapper.Books.Create(book);
+        }
+    }
+}
